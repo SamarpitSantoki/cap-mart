1081a23abeffb9826ddd81a2d93957eca8a34423
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-10-18 09:52:58 +0530

    Removed Strict Mode

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -5,8 +5,4 @@ import "./App.css";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React>
-    <App />
-  </React>
-);
+root.render(<App />);
