e3d2cbcd58d1aaae37dffa2f0534c66c66e11f9b
SamarpitSantoki <samarpit.santoki@gmail.com>
2022-12-02 18:15:00 +0530

    Some change in backend

--- server/index.ts
+++ server/index.ts
@@ -7,6 +7,7 @@ import fileupload from "express-fileupload";
 import path from "path";
 import dbConnect from "./db/connect";
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
